aab9a9d9c232f6688993ed9ac7c79d603df52cc0
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-01-16 11:51:08 +0300

    define custom chain ( bsc testnet )

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { MyComponent } from "./MyComponent";
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const bscTestnet = {
+  id: 97,
+  name: 'BSC Testnet',
+  network: 'bsc-testnet',
+  iconUrl: 'https://snowtrace.io/static/media/bsc.2b5b1b5e.svg',
+  iconBackground: '#f0b90b',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Binance Coin',
+    symbol: 'BNB',
+  },
+  rpcUrls: {
+    default: {
+    },
+  },
+  blockExplorers: {},
+  testnet: true,
+};
 export default function App() {
   return (
     <WagmiConfig client={wagmiClient}>
