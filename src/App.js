33d7407659b1e01d7dcc3d44d49a33bd658d601b
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-01-16 11:49:01 +0300

    create App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { MyComponent } from "./MyComponent";
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+export default function App() {
+  return (
+    <WagmiConfig client={wagmiClient}>
+  );
+}
