4620531b7d35ae03ebf99bf8dea08261b54ad922
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-01-25 11:06:42 +0300

    add hardhat config

--- src/App.js
+++ src/App.js
@@ -25,6 +25,27 @@ const bscTestnet = {
   testnet: true,
 };
 
+// development : this is the config to use for local hardhat network
+const hardhatLocalChain = {
+  id: 31337,
+  name: 'Hardhat Local Chain',
+  network: 'hardhat-local-chain',
+  iconUrl: 'https://snowtrace.io/static/media/bsc.2b5b1b5e.svg',
+  iconBackground: '#f0b90b',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Binance Coin',
+    symbol: 'BNB',
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://localhost:8545/'],
+    },
+  },
+  blockExplorers: {},
+  testnet: true,
+};
+
 const { provider, chains } = configureChains(
   [bscTestnet],
   [
